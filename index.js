078d18426438ca3cd52e04aebdcd2c984b5c939e
kenneth <ingkeneidel@gmail.com>
2022-07-26 11:30:28 -0500

    inicio back1

--- index.js
+++ index.js
@@ -17,12 +17,16 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
 
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
-});
+// // Syncing all the models at once.
+
+// conn.sync({ force: true }).then(() => {
+//   server.listen(3001, () => {
+//     console.log('%s listening at 3001'); // eslint-disable-line no-console
+//   });
+// });
+
+server.listen(process.env.PORT || 3002);
+console.log("Server on port", process.env.PORT || 3002);
